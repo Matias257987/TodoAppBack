fb57943f78e4b6122fe22eebef65c7e3e9528c2b
Matias <acostamatias25@gmail.com>
2025-05-31 19:40:25 -0300

    add: authRequest metod for autentication

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,19 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
+export interface AuthRequest extends Request {
+  user?: {
+    id: string;
+  };
+}
+
 export const verifyToken = async (
-  req: Request,
+  req: AuthRequest,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(" ")[1];
+
   if (!token) res.status(401).json({ message: "Access Denied" });
 
   try {
     const secret = process.env.JWT_SECRET || "secret";
-    const verified = jwt.verify(token!, secret);
-    (req as any).user = verified;
+    const verified = jwt.verify(token!, secret) as { id: string };
+    req.user = { id: verified.id };
     next();
   } catch (err) {
     res.status(403).json({ message: "Invalid Token" });
